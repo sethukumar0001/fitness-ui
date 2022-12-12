55cffaa539895d1dd43a016ba63d61a5c9ca6e06
sethu@curiotechnologies.in <sethu@curiotechnologies.in>
2022-12-12 19:01:00 +0530

    mobile responsive added

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { FiUser } from "react-icons/fi";
+import { FaTimes, FaBars } from "react-icons/fa";
+
 function Header(props: any) {
 	const [prevScrollPos, setPrevScrollPos] = useState(0);
 	const [visible, setVisible] = useState(true);
-	const router = useRouter()
+	const router = useRouter();
 	// new function:
 	const handleScroll = () => {
 		// find current scroll position
@@ -24,26 +27,21 @@ function Header(props: any) {
 		window.addEventListener("scroll", handleScroll);
 		return () => window.removeEventListener("scroll", handleScroll);
 	}, [prevScrollPos, visible, handleScroll]);
+
+	const [dropdownToggled, toggleDropdown] = useState(false);
+	const showNavbar = () => {
+		toggleDropdown(!dropdownToggled);
+	};
+
 	return (
-		<div
-			className={"header d-flex justify-content-between"}
-			style={{ top: visible ? "0" : "-120px" }}
-		>
-			<div className="image-div">
-				<Link href={`/`}>
-					<img src={`/images/2.png`} className="image" />
-				</Link>
-			</div>
-			<nav className="nav">
-				<ul className="d-flex">
+		<header className="header" style={{ top: visible ? "0" : "-120px" }}>
+			<img src={`/images/2.png`} className="logo" />
+			<nav className={`${dropdownToggled ? "responsive_nav" : ""}`}>
+				<ul>
 					<li>
 						<Link
 							href={"/"}
-							className={`${
-								router.pathname === `/`
-									? "active"
-									: ""
-							}`}
+							className={`${router.pathname === `/` ? "active" : ""}`}
 						>
 							The Workouts
 						</Link>
@@ -51,11 +49,7 @@ function Header(props: any) {
 					<li>
 						<Link
 							href={"/about"}
-							className={`${
-								router.pathname === `/about`
-									? "active"
-									: ""
-							}`}
+							className={`${router.pathname === `/about` ? "active" : ""}`}
 						>
 							Why SWEAT?
 						</Link>
@@ -63,11 +57,7 @@ function Header(props: any) {
 					<li>
 						<Link
 							href={"/try-sweat"}
-							className={`${
-								router.pathname === `/try-sweat`
-									? "active"
-									: ""
-							}`}
+							className={`${router.pathname === `/try-sweat` ? "active" : ""}`}
 						>
 							Try SWEAT
 						</Link>
@@ -75,11 +65,7 @@ function Header(props: any) {
 					<li>
 						<Link
 							href={"/offers"}
-							className={`${
-								router.pathname === `/offers`
-									? "active"
-									: ""
-							}`}
+							className={`${router.pathname === `/offers` ? "active" : ""}`}
 						>
 							Offers
 						</Link>
@@ -87,11 +73,7 @@ function Header(props: any) {
 					<li>
 						<Link
 							href={"/pricing"}
-							className={`${
-								router.pathname === `/pricing`
-									? "active"
-									: ""
-							}`}
+							className={`${router.pathname === `/pricing` ? "active" : ""}`}
 						>
 							Pricing
 						</Link>
@@ -99,21 +81,21 @@ function Header(props: any) {
 					<li>
 						<Link
 							href={"/contact-us"}
-							className={`${
-								router.pathname === `/contact-us`
-									? "active"
-									: ""
-							}`}
+							className={`${router.pathname === `/contact-us` ? "active" : ""}`}
 						>
 							Contact Us
 						</Link>
 					</li>
 				</ul>
+				<button className="nav-btn nav-close-btn" onClick={showNavbar}>
+					<FaTimes size={25} />
+				</button>
 			</nav>
-			<div className="profile-right">
-				<img src={`/images/profile.png`} className="image" />
-			</div>
-		</div>
+			<button className="nav-btn" onClick={showNavbar}>
+				<FaBars size={20} />
+			</button>
+			<FiUser className="profile-icon" size={30} />
+		</header>
 	);
 }
 
